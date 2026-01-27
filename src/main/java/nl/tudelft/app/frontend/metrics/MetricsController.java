2823d0887644744bea8793a1c216c7a3d6aca816
Frederik van der Els <fred@xpple.dev>
2026-01-27 17:11:07 +0100

    Remove Prometheus dependency

--- src/main/java/nl/tudelft/app/frontend/metrics/MetricsController.java
+++ src/main/java/nl/tudelft/app/frontend/metrics/MetricsController.java
@@ -1,27 +1,42 @@
 package nl.tudelft.app.frontend.metrics;
 
-import io.prometheus.client.Collector.MetricFamilySamples;
-import io.prometheus.client.exporter.common.TextFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.Enumeration;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static nl.tudelft.app.frontend.metrics.SmsMetrics.*;
 
 @RestController
 public class MetricsController {
 
+    private static final String RESPONSE_FORMAT = """
+        # HELP sms_requests_total Total number of SMS classification requests
+        # TYPE sms_requests_total counter
+        sms_requests_total{result="ham",} %s
+        sms_requests_total{result="spam",} %s
+        # HELP sms_requests_inflight Currently active SMS classification requests
+        # TYPE sms_requests_inflight gauge
+        sms_requests_inflight %s
+        # HELP sms_request_latency_seconds Latency for SMS classification requests
+        # TYPE sms_request_latency_seconds histogram
+        %s
+        sms_request_latency_seconds_bucket{endpoint="/sms",le="+Inf",} %s
+        sms_request_latency_seconds_count{endpoint="/sms",} %s
+        sms_request_latency_seconds_sum{endpoint="/sms",} %s
+        """;
+
     @GetMapping(value = "/metrics", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String metrics() throws IOException {
-        Writer writer = new StringWriter();
-        Enumeration<MetricFamilySamples> mfs = REGISTRY.metricFamilySamples();
-        TextFormat.write004(writer, mfs);
-        return writer.toString();
+    public String metrics() {
+        String smsLatencySecondsStr = IntStream.range(0, BUCKETS.size())
+            .mapToObj(i -> {
+                double bucket = BUCKETS.get(i);
+                double value = smsLatencySeconds[i];
+                return String.format("sms_request_latency_seconds_bucket{endpoint=\"/sms\",le=\"%s\",} %s", bucket, value);
+            })
+            .collect(Collectors.joining("\n"));
+        return RESPONSE_FORMAT.formatted(smsRequestsTotalHam, smsRequestsTotalSpam, inflightRequests, smsLatencySecondsStr, smsLatencySecondsCount, smsLatencySecondsCount, smsLatencySecondsSum);
     }
 }
-
